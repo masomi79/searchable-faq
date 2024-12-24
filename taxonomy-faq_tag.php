0a1ddb0d563fd080a951789f03c01f35c7f5ebcb
masomi79 <masomi79@gmail.com>
2024-12-24 12:17:36 +0900

    tag周りのスタイルの修正

--- taxonomy-faq_tag.php
+++ taxonomy-faq_tag.php
@@ -1,11 +1,11 @@
 <?php  get_header(); ?>
-<div class="site-main">
-    <h1>タグ：<?php single_term_title(); ?></h1>
+<div class="site-main has-global-padding faq-container">
+    <h2 class="faq_tag-title">FAQタグ:<?php single_term_title(); ?></h2>
     <div class="faq-list">
         <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
             <div class="faq-item">
-                <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
-                <div><?php the_excerpt(); ?></div>
+                <h3 class="faq-question"><?php the_title(); ?></h3>
+                <div class="faq-answer"><?php the_excerpt(); ?><a href="<?php the_permalink(); ?>">...詳細を見る</a></div>
             </div>
         <?php endwhile; else : ?>
             <p>このタグのついたFAQはありません。</p>
